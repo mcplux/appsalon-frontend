6554b43a80545568a133ce806a8df9226a75806d
Juan Martinez <pablomar01@outlook.com>
2024-02-20 10:00:30 -0500

    appointment layout created

--- src/views/appointments/AppointmentsApp.vue
+++ src/views/appointments/AppointmentsApp.vue
@@ -3,8 +3,28 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <h1>Appointment App</h1>
+  <div class="flex justify-between">
+    <h1 class="text-2xl lg:text-6xl font-black text-white">AppSalon</h1>
+
+    <div class="flex flex-col space-y-5">
+      <div class="flex gap-2 items-center">
+        <p class="text-white text-right">Hola, usuario</p>
+
+        <button type="button" class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">
+          Cerrar sesión
+        </button>
+      </div>
+
+      <nav class="flex gap-2 items-center justify-end">
+        <button class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg">
+          Mis citas
+        </button>
+
+        <RouterLink :to="{name: 'new-appointment'}" class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700">
+          Nueva cita
+        </RouterLink>
+      </nav>
+    </div>
   </div>
 
   <main>
