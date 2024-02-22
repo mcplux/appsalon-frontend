1b6b4e63b82d902727de274d53b4825358eac46a
Juan Martinez <pablomar01@outlook.com>
2024-02-22 17:04:06 -0500

    verify account

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,9 +1,33 @@
 <script setup>
+import { onMounted, inject } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import AuthAPI from '@/api/AuthAPI'
 
+const toast = inject('toast')
+const route = useRoute()
+const router = useRouter()
+const { token } = route.params
+
+onMounted(async () => {
+  try {
+    const { data } = await AuthAPI.verifyAccount(token)
+    toast.open({
+      message: data.msg,
+      type: 'success',
+    })
+
+    setTimeout(() => {
+      router.push({name: 'login'})
+    }, 1000);
+  } catch (err) {
+    toast.open({
+      message: err.response.data.msg,
+      type: 'error',
+    })
+  }
+})
 </script>
 
 <template>
-  <div>
-    <h1>Desde ConfirmAccountView</h1>
-  </div>
+  <h1 class="text-4xl lg:text-6xl font-extrabold text-white text-center mt-10">Verificar cuenta</h1>
 </template>
