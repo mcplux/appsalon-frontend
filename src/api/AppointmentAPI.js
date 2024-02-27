50176ff426d1ba2c31c131bf3f2304110e06d86a
Juan Martinez <pablomar01@outlook.com>
2024-02-27 12:00:52 -0500

    intercept axios request to add headers

--- src/api/AppointmentAPI.js
+++ src/api/AppointmentAPI.js
@@ -2,12 +2,6 @@ import api from "@/lib/axios"
 
 export default {
   create(data) {
-    const token = localStorage.getItem('AUTH_TOKEN')
-
-    return api.post('/appointments', data, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-  }
+    return api.post('/appointments', data)
+  },
 }
